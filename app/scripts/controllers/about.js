1debba81d2e633b06244023f088e20b1ef8ee182
Serena <serena.magliulo@gmail.com>
2018-07-11 12:47:36 +0100

    Update design

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,36 +8,35 @@
  * Controller of the potatoProjectApp
  */
 angular.module('potatoProjectApp')
- .filter('author', function() {
+  .filter('author', function() {
     return function(input) {
       var separator = '"';
       var arrayOfStrings = input.split(separator);
       if (arrayOfStrings.length >= 2) {
         return arrayOfStrings[1]
       } else {
-        return arrayOfStrings[0];  
+        return arrayOfStrings[0];
       }
-      
+
     };
   })
 
   .filter('tags', function() {
     return function(input) {
-      var tagsSeparator = '"';
-      var arrayOfStrings = input.split(tagsSeparator);
-      
-        return arrayOfStrings[0];
-        console.log(arrayOfStrings);
-      
+      var tagsSeparator = ' ';
+      var arrayOfStrings = input.split(tagsSeparator,3);
+      return arrayOfStrings;
+      console.log(arrayOfStrings);
+
     };
   })
-  .controller('AboutCtrl', ['$window', '$scope', function ($window, $scope) {
+  .controller('AboutCtrl', ['$window', '$scope', function($window, $scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    var data= $window.sessionStorage.data;
+    var data = $window.sessionStorage.data;
     $scope.feed = JSON.parse(data);
     console.log("data = ", $scope.feed);
 
